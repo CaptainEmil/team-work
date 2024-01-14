0b2aa19e94b54bf1410d82807afc532a24133b5f
Emil Mirzazada <mirzazadeemil2005@gmail.com>
2024-01-14 21:00:50 +0400

    added js for checking username and password

--- pages/sign up/signup.js
+++ pages/sign up/signup.js
@@ -1,10 +1,33 @@
 const form = document.querySelector('form');
 const usernameInput = document.querySelector('#username');
 const passwordInput = document.querySelector('#password');
+const usernameError = document.querySelector('#username-error');
+const passwordError = document.querySelector('#password-error');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     cnt = 0;
+    isInvalid = false;
+
+    if (usernameInput.value === '') {
+        usernameError.style.display = 'inline';
+        isInvalid = true;
+    }
+    else {
+        usernameError.style.display = 'none';
+    }
+    if (passwordInput.value === '') {
+        passwordError.style.display = 'inline';
+        isInvalid = true;
+    }
+    else {
+        passwordError.style.display = 'none';
+    }
+
+    if (isInvalid) return;
+
+    usernameError.style.display = 'none';
+    passwordError.style.display = 'none';
 
     while (localStorage.getItem(`user${cnt++}`) !== null) { };
 
